51ac80178de50529f795bac13153da68a45f9b36
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2016-07-02 08:46:58 +0900

    Modify AdminUser to remove modules recoverable, rememberable

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,8 +1,6 @@
 class AdminUser < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :omniauthable,
-         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :database_authenticatable, :omniauthable, :trackable, :validatable
 
   with_options presence: true do
     validates :email
