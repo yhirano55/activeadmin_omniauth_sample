9ee1893c6e848e6f33bc216f8e76eb321e5e6bb2
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2016-07-02 09:08:21 +0900

    Modify admin/admin_users to disable to create record

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  actions :index, :show
 
   index do
     selectable_column
@@ -15,14 +15,4 @@ ActiveAdmin.register AdminUser do
   filter :current_sign_in_at
   filter :sign_in_count
   filter :created_at
-
-  form do |f|
-    f.inputs "Admin Details" do
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
-    end
-    f.actions
-  end
-
 end
